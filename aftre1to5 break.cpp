e1a7d97cc5f0775fe5a6e4ccd502f64b8678ac81
girajalasainaidu-bit <girajalasainaidu@gmail.com>
2025-10-30 13:20:03 +0530

    Add files via upload

---
+++ aftre1to5 break.cpp
@@ -0,0 +1,18 @@
+//WAP A PRINT 1 TO 10 AND USE BREAK AFTER .
+#include<stdio.h>
+int main()
+{
+	for(int i=1;i<10;i++)
+	
+	{
+		printf("%d",i);
+		if(i==5)
+		{
+		break;
+		}
+	} 
+	return 0;
+}
+ 
+
+
